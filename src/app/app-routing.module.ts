ec312c5f33645eb6451ca7ea4b8685686fae9a14
Aaditi Harale <aaditiharale@Aaditis-MacBook-Air.local>
2023-03-30 23:52:27 +0530

    layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FilterSearchComponent } from './component/filter-search/filter-search.component';
-import { MainSectionComponent } from './component/main-section/main-section.component';
-import { NavbarComponent } from './component/navbar/navbar.component';
-import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { AllOutreachCircleComponent } from './pages/all-outreach-circle/all-outreach-circle.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { UserComponent } from './pages/user/user.component';
 
 const routes: Routes = [
-  { path : "" , component : LoginComponent},
-  { path : "navbar" , component : NavbarComponent},
-  { path : "sidebar" , component : SidebarComponent},
-  { path : "filter" , component : FilterSearchComponent},
-  { path : "main" , component : MainSectionComponent},
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+        children: [{ path: '', component: AllOutreachCircleComponent } ]
+      }
+    ],
+  },
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
