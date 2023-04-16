7231b53b94e80efcd46fb2212afbb93323ca276a
shingnapure <shilpa@politicaldata.com>
2023-04-16 14:51:02 +0530

    improve in css

--- src/app/component/create-group-model/create-group-model.component.ts
+++ src/app/component/create-group-model/create-group-model.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentInit, AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-group-model',
@@ -7,14 +8,17 @@ import { AfterContentInit, AfterViewInit, Component, OnInit, ViewChild } from '@
 })
 export class CreateGroupModelComponent implements OnInit  {
 
-  constructor() { }
+  constructor(private dialogRef : MatDialogRef<CreateGroupModelComponent>) { }
   ngOnInit(): void {
   }
-  
   name : string ;
   
+  handleClose(){
+    this.dialogRef.close()
+  }
+
   createGroup(){
-    alert('h')
+    alert('helo')
   }
   
   
