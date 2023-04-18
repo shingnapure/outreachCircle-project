64e90fe051e7ef9a82aec0417d1c0bf4e1be3fe7
shingnapure <shilpa@politicaldata.com>
2023-04-18 12:57:37 +0530

    changes done in sidebar

--- src/app/component/sidebar/sidebar.component.ts
+++ src/app/component/sidebar/sidebar.component.ts
@@ -1,9 +1,5 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Apollo } from 'apollo-angular';
-import { CookieService } from 'ngx-cookie-service';
-import { outreachByAlias } from 'src/app/graphql/graphql.query';
-import { DataService } from 'src/app/services/data.service';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -11,26 +7,15 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit  {
-  constructor(private router : Router , private apollo : Apollo , private cookies : CookieService , private dataService : DataService) {
+  constructor() {
     
   }
 
-  linkId;
-  name ; 
-  ngOnInit(): void {
-    this.linkId = this.router.url.split('/')[2]
+  @Input() linkId : string;
+  @Input() name : string;
 
-    this.apollo.query<any>({
-      query : outreachByAlias ,
-      variables : {
-        "alias": this.linkId
-      },
-      context: {
-        headers: { Authorization: this.cookies.get(this.dataService.token) },
-      }
-    }).subscribe(({ data }) => {
-      this.name = data.outreachCircleByAlias.name
-    });
+  ngOnInit(): void {
+    
 
   }
   
